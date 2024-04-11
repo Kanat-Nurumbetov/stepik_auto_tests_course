a759bb4aed31123637b6adeb6a3db22cd1e00c13
Kanat-Nurumbetov <130391084+Kanat-Nurumbetov@users.noreply.github.com>
2024-04-11 22:27:02 +0500

    Update function.py

--- function.py
+++ function.py
@@ -4,8 +4,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 def login(browser):
     browser.implicitly_wait(5)
-    browser.find_element(By.ID, "id_login_email").send_keys("orklik@gmail.com")
-    browser.find_element(By.ID, "id_login_password").send_keys("rfyf1987")
+    browser.find_element(By.ID, "id_login_email").send_keys("")
+    browser.find_element(By.ID, "id_login_password").send_keys("")
     browser.find_element(By.XPATH, '//*[@id="login_form"]/button').click()
     
 def type_answer():
